8c30e09a9e78cc820ec7ac4f206cab9d86794929
BrunoRodriguesN <bruno_adeira@hotmail.com>
2021-05-26 14:29:43 -0300

    Order , instant, iso8601 Parte II

---
+++ src/main/java/com/bruno/course/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.bruno.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bruno.course.entities.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
