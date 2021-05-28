45fe37723719aef440267aa0a3e5ac3c560a8a35
BrunoRodriguesN <bruno_adeira@hotmail.com>
2021-05-28 12:30:52 -0300

    User delete

--- src/main/java/com/bruno/course/services/UserService.java
+++ src/main/java/com/bruno/course/services/UserService.java
@@ -14,18 +14,21 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
-	public User findById(Long id){
-		Optional<User> obj  = userRepository.findById(id);
+
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
 		return obj.get();
 	}
-	
-	public User insert (User obj) {
+
+	public User insert(User obj) {
 		return userRepository.save(obj);
 	}
-	
+
+	public void delete(Long id) {
+	   userRepository.deleteById(id);
+	}
 }
