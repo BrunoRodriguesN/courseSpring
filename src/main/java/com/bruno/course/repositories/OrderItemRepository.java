cc243763770154fe68aa0b752cf4616b16f99aea
BrunoRodriguesN <bruno_adeira@hotmail.com>
2021-05-28 09:46:37 -0300

    OrderItem, manytomany association with extra attributes

---
+++ src/main/java/com/bruno/course/repositories/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package com.bruno.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bruno.course.entities.OrderItem;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}
